bf0ea454f4ccc72ce253d6ac0b11a8f48fbec061
Frederick Chon <frederick.chon@gmail.com>
2019-04-18 11:29:14 -0700

    first commit

---
+++ build/precache-manifest.ca3a0f257998b464949ee5d653baba71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4fdccbf3cc694f2cd55",
+    "url": "/static/css/main.1415eeec.chunk.css"
+  },
+  {
+    "revision": "e4fdccbf3cc694f2cd55",
+    "url": "/static/js/main.9cb62d38.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee9acda9e92d3f6a74ba",
+    "url": "/static/css/2.bbf7962c.chunk.css"
+  },
+  {
+    "revision": "ee9acda9e92d3f6a74ba",
+    "url": "/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "53daf5128dce7f78af3c2c01c2147864",
+    "url": "/index.html"
+  }
+];
